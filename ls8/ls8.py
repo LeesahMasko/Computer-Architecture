5e975e3ebe510b50155602a7b83adb2e06f472dd
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-07-20 19:23:47 -0700

    Made program dynamic

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -6,6 +6,15 @@ import sys
 from cpu import *
 
 cpu = CPU()
-
-cpu.load()
+program = [
+      # From print8.ls8
+      0b10000010, # LDI R0,8
+      0b00000000,
+      0b00001000,
+      0b01000111, # PRN R0
+      0b00000000,
+      0b00000001, # HLT
+  ]
+cpu.load(program)
+cpu.run()
 cpu.run()
